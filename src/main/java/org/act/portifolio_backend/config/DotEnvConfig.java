507f7b91c6ad420865e01a2104d23c18affd180b
Abelo73 <abeladisu73@gmail.com>
2025-05-07 17:01:21 +0300

    removed dotenv file

--- src/main/java/org/act/portifolio_backend/config/DotEnvConfig.java
+++ src/main/java/org/act/portifolio_backend/config/DotEnvConfig.java
@@ -1,15 +1,15 @@
-package org.act.portifolio_backend.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DotEnvConfig {
-
-    public DotEnvConfig(){
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
-    }
-
-}
+//package org.act.portifolio_backend.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DotEnvConfig {
+//
+//    public DotEnvConfig(){
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//
+//    }
+//
+//}
